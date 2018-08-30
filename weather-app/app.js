f1bfb726d53825d516ed9132e15d38f5918755d4
ThePuffin <ballutvincent@gmail.com>
2018-08-30 11:19:29 +0200

    lecture lat et long

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,9 @@ request({
     url: "https://maps.googleapis.com/maps/api/geocode/json?address=17%20rue%20delandine%20lyon",
     json: true, //transform json en objet
 }, (error, response, body)=>{
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
+    // console.log(`Address: ${body.results[0].formatted_address}`);
+    // console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    // console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+
 })
