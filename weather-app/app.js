07f2b163e44d4d71ce07c06340697d641f593d02
ThePuffin <ballutvincent@gmail.com>
2018-08-30 13:23:46 +0200

    ajout de message d'erreurs

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,38 @@
-const request = require('request');
+const request = require('request')
+const yargs = require('yargs')
 
-request({
-    url: "https://maps.googleapis.com/maps/api/geocode/json?address=17%20rue%20delandine%20lyon",
-    json: true, //transform json en objet
-}, (error, response, body)=>{
-    console.log(JSON.stringify(body, undefined, 2));
-    // console.log(`Address: ${body.results[0].formatted_address}`);
-    // console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-    // console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+const argv = yargs
+  .option({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h').argv;
 
-})
+console.log(argv.a);
+const encodeAddress = encodeURIComponent(argv.a)
+
+request(
+  {
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`,
+    json: true // transform json en objet
+  },(error, response, body) => {
+    if(error){
+        console.log('unable to connect to google servers');
+    }
+    else if (body.status === "ZERO_RESULTS"){
+        console.log('unable to find that adress.');
+    }
+    else if (body.status=== "OK"){
+    // console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+    }
+
+  }
+)
--- weather-app/playground/callbacks.js
+++ weather-app/playground/callbacks.js
@@ -1,12 +1,12 @@
-let getUser = (id, callback) =>{
-    const user = {
-        id: id,
-        name: 'Vikram',
-    }
-    //setTimeout(()=>{ callback(user);}, 3000)
-    callback(user);
-} ;
+let getUser = (id, callback) => {
+  const user = {
+    id: id,
+    name: 'Vikram'
+  }
+  // setTimeout(()=>{ callback(user);}, 3000)
+  callback(user)
+}
 
-getUser(31, (userObject)=>{
-    console.log(userObject);
+getUser(31, userObject => {
+  console.log(userObject)
 })
